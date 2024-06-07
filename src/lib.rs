2651d6fef72e7ff1a0233eb38ac5aca985bffc13
LukasGerm <lukas@lukasgermerott.de>
2024-06-07 14:12:36 +0200

    add outcome

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![allow(clippy::not_unsafe_ptr_arg_deref)]
 
+/**
+ * https://astexplorer.net/#/gist/e931bbd1350f930ec594e101d6a9a2b7/c992bb3545f0749cf34e4a90e3fdd734894f674e outcome
+ */
+
 use swc_common::{SourceMapper, Spanned};
 use swc_core::{
     common::FileName,
