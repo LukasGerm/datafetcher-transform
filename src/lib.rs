a07e5c8d31c6918f8df0e4982b87343598344b2a
LukasGerm <lukas@lukasgermerott.de>
2024-06-07 14:25:18 +0200

    add some dummy transform

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
-#![allow(clippy::not_unsafe_ptr_arg_deref)]
-
 /**
  * https://astexplorer.net/#/gist/e931bbd1350f930ec594e101d6a9a2b7/c992bb3545f0749cf34e4a90e3fdd734894f674e outcome
  */
 
-use swc_common::{SourceMapper, Spanned};
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 use swc_core::{
-    common::FileName,
-    ecma::{ast::Program, visit::VisitMutWith},
-    plugin::{
-        metadata::TransformPluginMetadataContextKind,
-        plugin_transform,
-        proxies::{PluginCommentsProxy, TransformPluginProgramMetadata},
-    },
+    ecma::{ast::Program, visit::FoldWith},
+    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
 };
 
 #[plugin_transform]
-fn datafetcher_transform_ast(mut program: Program, data: TransformPluginProgramMetadata) -> Program {
-    println!("datafetcher_transform_ast");
+fn swc_plugin(program: Program, data: TransformPluginProgramMetadata) -> Program {
+    let config = serde_json::from_str::<Option<datafetcher_transform::Config>>(
+        &data
+            .get_transform_plugin_config()
+            .expect("failed to get plugin config for react-remove-properties"),
+    )
+    .expect("invalid packages")
+    .unwrap_or(datafetcher_transform::Config::All(true));
 
-    program
-
+    program.fold_with(&mut datafetcher_transform::datafetcher_transform(
+        config,
+    ))
 }
